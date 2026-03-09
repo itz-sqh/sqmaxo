3d8b7f5ef65f09143de61e6583f0c3a96db85d43
a <a>
2026-03-09 22:42:21 +1100

    open window

---
+++ Makefile
@@ -0,0 +1,21 @@
+CC = clang
+CFLAGS = -std=c99 -Wall -Wextra -Wpedantic ${shell pkg-config --cflags x11}
+LFLAGS = ${shell pkg-config --libs x11}
+TARGET = a.out
+SRC = main.c
+OBJ = ${SRC:.c=.o}
+
+all: ${TARGET}
+
+run: ${TARGET}
+	./${TARGET}
+
+${TARGET}: ${OBJ}
+	${CC} -o $@ $^ ${LFLAGS}
+%.o: %.c
+	${CC} ${CFLAGS} -c $< -o $@
+
+clean:
+	git clean -fdx
+
+.PHONY: all run clean
--- main.c
+++ main.c
@@ -1,8 +1,43 @@
+#include <X11/Xlib.h>
+
 #include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
 
 int
 main(void)
 {
-  printf("hello\n");
+  Display *dpy;
+  Window w;
+  GC gc;
+  XEvent e;
+  int black, white, scr_no, root;
+
+  dpy = XOpenDisplay(NULL);
+  assert(dpy);
+  scr_no = DefaultScreen(dpy);
+  root = DefaultRootWindow(dpy);
+  black = BlackPixel(dpy, scr_no);
+  white = WhitePixel(dpy, scr_no);
+  w = XCreateSimpleWindow(dpy, root, 0, 0, 271, 67, 0, black, black);
+  XSelectInput(dpy, w, KeyPressMask | ExposureMask | StructureNotifyMask);
+  XMapWindow(dpy, w);
+  gc = XCreateGC(dpy, w, 0, NULL);
+  XSetForeground(dpy, gc, white);
+  do {
+    XNextEvent(dpy, &e);
+  } while (e.type != MapNotify);
+  XDrawLine(dpy, w, gc, 90, 22, 180, 44);
+  XFlush(dpy);
+  for (;;) {
+    XNextEvent(dpy, &e);
+    if (e.type == Expose)
+      XDrawLine(dpy, w, gc, 90, 22, 180, 44);
+    if (e.type == KeyPress)
+      break;
+  }
+  XFreeGC(dpy, gc);
+  XDestroyWindow(dpy, w);
+  XCloseDisplay(dpy);
   return 0;
 }
