9b341e1bbb31c0f73bb8c484171ae781102e2ca2
itz-sqh <itz.sqh@gmail.com>
2026-03-12 21:32:20 +0300

    remove dead code, loading and saving files, wrap C-b and C-f

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ Window w;
 GC gc;
 int black, white;
 int alive = 1;
-char text_buffer[1024];
+static char text_buffer[65536], cur_filename[FILENAME_MAX]; /* keep on bss, cur_filename unused for now */
 int buf_len = 0, cursor = 0;
 int x, y, font_width, offset;
 FILE *file;
@@ -44,7 +44,7 @@ init_font(void)
 {
   Font fnt;
   XFontStruct *fs;
-  const char *fontname = "10x20";
+  const char *fontname = "fixed";
 
   fnt = XLoadFont(dpy, fontname);
   XSetFont(dpy, gc, fnt);
@@ -53,7 +53,7 @@ init_font(void)
 }
 
 void
-redraw(void)
+redraw_buffer(void)
 {
   int x_ = offset, y_ = offset, i;
 
@@ -70,15 +70,9 @@ redraw(void)
 }
 
 void
-delete_at_point(void)
-{
-  XClearArea(dpy, w, x, y - 2 * font_width, 2 * font_width, 4 * font_width, False);
-}
-
-void
 draw_cursor(void)
 {
-  XFillRectangle(dpy, w, gc, x, y - 2 * font_width, font_width / 3, 2 * font_width);
+  XFillRectangle(dpy, w, gc, x, y - offset, font_width / 3, 2 * font_width);
 }
 
 void
@@ -99,24 +93,6 @@ goto_start_of_line(void)
   while (cursor > 0 && text_buffer[cursor - 1] != '\n')
     --cursor;
   x = offset;
-}
-
-void
-forward_char(void)
-{
-  if (cursor < buf_len) {
-    ++cursor;
-    x += font_width;
-  }
-}
-
-void
-backward_char(void)
-{
-  if (cursor > 0 && text_buffer[cursor - 1] != '\n') {
-    --cursor;
-    x -= font_width;
-  }
 }
 
 void
@@ -141,7 +117,7 @@ prev_line(void)
 }
 
 void
-down_line(void)
+next_line(void)
 {
   int i = cursor, col, col_cur = 0, col_next = 0;
 
@@ -165,6 +141,65 @@ down_line(void)
 }
 
 void
+forward_char(void)
+{
+  if (cursor < buf_len) {
+    if (text_buffer[cursor] == '\n') {
+      y += offset;
+      x = offset;
+    } else {
+      x += font_width;
+    }
+    ++cursor;
+  }
+}
+
+void
+backward_char(void)
+{
+  int col = 0, i;
+
+  if (cursor > 0) {
+    if (text_buffer[cursor - 1] == '\n') {
+      for (i = cursor - 1; i > 0 && text_buffer[i - 1] != '\n'; --i)
+        ++col;
+      y -= offset;
+      x = offset + col * font_width;
+    } else {
+      x -= font_width;
+    }
+    --cursor;
+  }
+}
+
+int
+load_from_or_create_file(const char *filename)
+{
+  if ((file = fopen(filename, "r")) == NULL) {
+    if ((file = fopen(filename, "w")) == NULL)
+      return 0;
+  } else {
+    fseek(file, 0, SEEK_END);
+    buf_len = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    fread(text_buffer, 1, buf_len, file);
+  }
+  fclose(file);
+  return 1;
+}
+
+int
+save_to_file(const char *filename)
+{
+  if ((file = fopen(filename, "w")) == NULL)
+    return 0;
+  if (fwrite(text_buffer, 1, buf_len, file) <= 0)
+    return 0;
+  fclose(file);
+  return 1;
+}
+
+void
 handle_ctrl(KeySym key_sym)
 {
   switch (key_sym) {
@@ -173,7 +208,7 @@ handle_ctrl(KeySym key_sym)
     alive = 0;
     break;
   case XK_s:
-    if ((fwrite(text_buffer, 1, buf_len, file)) <= 0)
+    if (save_to_file(cur_filename) == 0)
       fprintf(stderr, "cannot write to file\n");
     break;
   case XK_a:
@@ -192,13 +227,11 @@ handle_ctrl(KeySym key_sym)
     prev_line();
     break;
   case XK_n:
-    down_line();
+    next_line();
     break;
   default:
     break;
   }
-  redraw();
-  draw_cursor();
 }
 
 void
@@ -220,8 +253,8 @@ event_loop(void)
     XNextEvent(dpy, &e);
     switch (e.type) {
     case Expose:
+      redraw_buffer();
       draw_cursor();
-      XDrawString(dpy, w, gc, offset, offset, text_buffer, buf_len);
       break;
     case KeyPress:
       key_event = &e.xkey;
@@ -229,7 +262,6 @@ event_loop(void)
       if ((key_event->state & ControlMask)) {
         handle_ctrl(key_sym);
       } else {
-        delete_at_point();
         len = XLookupString(key_event, buf, sizeof(buf), &key_sym, NULL);
         switch (key_sym) {
         case XK_Return:
@@ -246,7 +278,6 @@ event_loop(void)
             if (text_buffer[cursor - 1] == '\n')
               backline = 1;
             memmove(text_buffer + cursor - 1, text_buffer + cursor, buf_len - cursor);
-            delete_at_point();
             --buf_len;
             --cursor;
             if (backline) {
@@ -267,7 +298,7 @@ event_loop(void)
           prev_line();
           break;
         case XK_Down:
-          down_line();
+          next_line();
           break;
         default:
           if (len > 0) {
@@ -278,10 +309,10 @@ event_loop(void)
             x += font_width;
           }
         }
-        redraw();
-        draw_cursor();
-        XFlush(dpy);
       }
+      redraw_buffer();
+      draw_cursor();
+      XFlush(dpy);
       break;
     default:
       break;
@@ -304,9 +335,10 @@ main(int argc, char *argv[])
     fprintf(stderr, "missing file name\n");
     return 1;
   }
-  if ((file = fopen(argv[1], "w")) == NULL) {
-    fprintf(stderr, "cannot open %s for writing\n", argv[1]);
-    return 1;
+  strcpy(cur_filename, argv[1]);
+  if ((load_from_or_create_file(cur_filename)) == 0) {
+    fprintf(stderr, "cannot open %s\n", cur_filename);
+    exit(1);
   }
   init_window();
   init_gc();
